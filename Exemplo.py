4adad5e1184c2dabd34f1514884b45ab2084f336
Lucas2000-student <rm560179@fiap.com.br>
2024-10-10 21:43:32 -0300

    Comando For

--- Exemplo.py
+++ Exemplo.py
@@ -1,4 +1,6 @@
-a = int(input('Digite o priemiro valor: '))
-b = int(input('Digite o priemiro valor: '))
-s = a + b
-print(f'Após usar a calculadora on line, posso te dizer com certeza que {a} + {b} = {s}') 
+for i in range(11):
+    if(i==7):
+        continue
+    print(i)
+    if(i==8):
+        break
