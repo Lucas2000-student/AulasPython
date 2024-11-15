1e4a39dd0c51e2f4c8f5a67c81cdd92b389b95f1
Lucas2000-student <rm560179@fiap.com.br>
2024-11-14 21:30:55 -0300

    inicio

---
+++ GlobalSolution.py
@@ -0,0 +1,10 @@
+n = 0
+while (n != 4):
+    print('--- Bem vindo ao Solar Connect! ---\n')
+    print('1 - Informações do Serviço.')
+    print('2 - Realizar Cadastro.')
+    print('3 - Vizualizar Pessoas cadastradas.')
+    print('4 -  Sair.\n')
+    n = int(input('Digite a opção desejada: '))
+    print('\n')
+print('Obrigado por Utilizar nossos serviços!')
