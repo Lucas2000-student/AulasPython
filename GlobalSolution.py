2e83360fbbbe7c54221dd543fc8c38428fcf71ee
Lucas2000-student <rm560179@fiap.com.br>
2024-11-18 16:54:12 -0300

    Inicio

--- GlobalSolution.py
+++ GlobalSolution.py
@@ -7,4 +7,23 @@ while (n != 4):
     print('4 -  Sair.\n')
     n = int(input('Digite a opção desejada: '))
     print('\n')
+
+    if(n == 1):
+        print('--- Sobre a Solar Connect ---')
+        print('Criamos está empresa com a ideia de ajudar prédios/condomínios a economizar na conta de energia e conhecer a energia solar.')
+        print('Para isso, nós efetuamos um cálculo de custo energético total de todos os moradores por 1 mês.')
+        print('Depois disso, efetuamos o levantamento de custo para a instalação de todos os equipamentos para suprir o gasto de energia.')
+        print('Depois de tudo instalado, você e seus vizinhos poderam ver as informações de gastos e uso de energia.')
+        print('Podendo efetuar a transferência de energia entre um ao outro.')
+        input()
+    elif(n == 2):
+        print('--- Área de Cadastro ---')
+        tipo = input('Irá cadastrar um condomínio ou Apartamento: ')
+        j = int(input('Digite a quantidade de membros para o cadastro: '))
+        
+        input()
+    elif(n == 3):
+        print('--- Sobre a Solar Connect ---')
+        input()
+
 print('Obrigado por Utilizar nossos serviços!')
