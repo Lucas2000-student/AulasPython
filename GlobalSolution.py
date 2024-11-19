1288571b4832dd7b0eee6ab6f7a54403f7b5f138
Lucas2000-student <rm560179@fiap.com.br>
2024-11-19 00:49:56 -0300

    finalizado

--- GlobalSolution.py
+++ GlobalSolution.py
@@ -1,4 +1,12 @@
+condominio = []
+predio =[]
+
 n = 0
+j = 0
+h = 0
+aptos = 0
+casas = 0
+
 while (n != 4):
     print('--- Bem vindo ao Solar Connect! ---\n')
     print('1 - Informações do Serviço.')
@@ -18,12 +26,47 @@ while (n != 4):
         input()
     elif(n == 2):
         print('--- Área de Cadastro ---')
-        tipo = input('Irá cadastrar um condomínio ou Apartamento: ')
-        j = int(input('Digite a quantidade de membros para o cadastro: '))
-        
-        input()
+        print('Qual o tipo de localidade?')
+        print('1 - Prédio. \n2 - Condomínio.')
+        j = int(input('Digite: '))
+        if (j == 1):
+            quant = int(input('Quantos Aptos: '))
+            aptos = aptos + quant
+            for i in range(quant):
+                nome = input('Nome do dono: ')
+                sobrenome = input('Sobrenome: ')
+                apto = input('N° do Apto: ')
+                moradores = input('N° de moradores: ')
+                predio.append('Dono(a): ' + nome + ' ' + sobrenome + 'mora no apto ' + apto + ' em ' + moradores + 'pessoa(s)')
+                print('\n')
+            print('Cadastros concluídos!')
+            input()
+        elif(j == 2):
+            quant = int(input('Quantas casas: '))
+            casas = casas + quant
+            for i in range(quant):
+                nome = input('Nome do dono: ')
+                sobrenome = input('Sobrenome: ')
+                casa = (input('N° da casa: '))
+                moradores = (input('N° de moradores: '))
+                condominio.append('Dono(a): ' + nome + ' ' + sobrenome + 'mora na casa ' + casa + ' em ' + moradores + 'pessoa(s)')
+                print('\n')
+            print('Cadastros concluídos!')
+            input()
     elif(n == 3):
-        print('--- Sobre a Solar Connect ---')
+        print('--- Área de visualização: ---')
+        if (aptos > 0):
+            print('Moradores do Prédio:')
+            for u in range(aptos):
+                print(predio(h))
+                h = h + 1
+            h = 0
+        if (casas > 0):
+            print('Moradores do Condomínio:')
+            for u in range(casas):
+                print(condominio(h))
+                h = h + 1
+            h = 0
         input()
 
 print('Obrigado por Utilizar nossos serviços!')
