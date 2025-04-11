4fdbc35a33ec340e525354d2bcc450d1da13f292
Lucas2000-student <rm560179@fiap.com.br>
2025-04-10 22:03:27 -0300

    fim do exercício

---
+++ Ex43.py
@@ -0,0 +1,29 @@
+arquivo = open("D:\\baseDados.txt", "r")
+conteudo = arquivo.read()
+baseDados = conteudo.split(";")
+listaCliente = []
+listaProduto = []
+
+for i in baseDados:
+    if (i.startswith("C")):
+        infoC = i.split(",")
+        nome = infoC[0]
+        nome.replace('C',':')
+        idade = infoC[1]
+        cidade = infoC[2]
+        Cliente = {'nome': nome,'idade': idade, 'cidade': cidade}
+        listaCliente.append(Cliente)
+    else:
+        infoP = i.split(",")
+        nome = infoP[0]
+        nome.replace('P',':')
+        quant = infoP[1]
+        preco = infoP[2]
+        Produto = {'nome': nome,'quant': quant, 'preco': preco}
+        listaProduto.append(Produto)
+
+arquivo = open("D:\\clienteProduto.txt", "a")
+for i in listaCliente:
+    arquivo.write(f"Nome: {i.get('nome')[1:]}\nIdade: {i.get('idade')}\nCidade: {i.get('cidade')}\n\n")
+for i in listaProduto:
+    arquivo.write(f"Nome: {i.get('nome')[1:]}\nEstoque: {i.get('quant')}\nPreço: R${i.get('preco')}\n\n")
